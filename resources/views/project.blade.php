5347d1e739ec08dca0592c8e330d862fc23451f9
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-04-16 15:51:07 -0400

    display tweaks and data in

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -13,7 +13,7 @@
         </div>
     @endif
 
-    <p><{{$project->content}}</p>
+    <p>{{$project->content}}</p>
 
     @if ($project->url)
         View Project: <a href="{{$project->url}}">{{$project->url}}</a>
@@ -22,7 +22,7 @@
     <p>
         Posted: {{$project->created_at->format('M j, Y')}}
         <br>
-        Type: {{$project->type->title}}
+      
     </p>
 
     <a href="/">Back to Home</a>
