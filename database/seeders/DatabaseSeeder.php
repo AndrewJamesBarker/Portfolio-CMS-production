97f93781fa097cfb2007a1868363bddb2cd18af6
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-04-15 22:01:26 -0400

    skills added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Type;
 use App\Models\Project;
+use App\Models\Skill;
 use App\Models\Education;
 use App\Models\Employment;
 use Illuminate\Database\Seeder;
@@ -22,12 +23,17 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Type::truncate();
         Project::truncate();
+        Skill::truncate();
         Education::truncate();
         Employment::truncate();
         
         User::factory()->count(2)->create();
         Type::factory()->count(3)->create();
         Project::factory()->count(4)->create();
+        Skill::factory()->count(14)->create()->each(function($skill){
+            $projects = Project::all()->random(rand(1,2) )->pluck('id');
+            $skill->projects()->attach($projects);
+        });
         Education::factory()->count(2)->create();
         Employment::factory()->count(3)->create();
             
