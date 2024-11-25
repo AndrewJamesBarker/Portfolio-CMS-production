38225496874df03a0f0a608efe61919398d1fa57
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2024-11-25 09:36:37 -0500

    alter welcome msg.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
     <h2 class="w3-text-blue">About Me</h2>
 
     <p>
-    I'm a Full Stack Developer with a sharp eye for front-end design and a knack for solving complex problems. My experience includes working with companies that rely on Drupal and React to build dynamic, user-centric web applications. I thrive on creating solutions that are not only functional but also intuitive and visually engaging.
-    I'm passionate about creating user-friendly, intuitive, visually appealing web applications. I'm also experienced in developing back-end systems and databases, and I'm comfortable working with various programming languages and frameworks. I'm always looking for ways to improve my skills and stay up-to-date with the latest technologies. I'm a team player who enjoys collaborating to create innovative solutions.
+    I'm a Full Stack Developer with an eye for front-end design, experience in developing back-end systems and databases, and a knack for solving complex problems. My experience includes working with companies that rely on Drupal and React to build dynamic, user-centric web applications. I thrive on creating solutions that are not only functional but also intuitive and visually engaging.
+    I'm always looking for ways to improve my skills and stay up-to-date with the latest technologies. I'm a team player who enjoys collaborating to create innovative solutions.
     </p>
 
 </section>
